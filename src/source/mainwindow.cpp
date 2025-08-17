7d51b4e9ce9786c6cc2f2f0eecab665d5dd38db3
Nurislam Iarmukhamedov <nuryarmukhamedov@mail.ru>
2025-08-17 20:49:38 +0300

    Notes container created

--- src/source/mainwindow.cpp
+++ src/source/mainwindow.cpp
@@ -4,12 +4,17 @@
 #include "addnote.h"
 #include <QMessageBox>
 #include <QScrollArea>
+#include <QScrollBar>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
+    this->setWindowTitle("NOTEBOOK::IO");
+    ui->notes_scroll_area->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+    ui->notes_scroll_area->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    ui->notes_scroll_area->setLayoutDirection(Qt::RightToLeft);
+    ui->notes_scroll_area->verticalScrollBar()->setLayoutDirection(Qt::LeftToRight);
 }
 
 MainWindow::~MainWindow()
@@ -20,7 +25,8 @@ MainWindow::~MainWindow()
 
 void MainWindow::add_note(const QString &shortText, const QString &fullText) {
     auto *note = new NoteLabel(shortText, fullText, this);
-    note->show();
+    note->setStyleSheet("color: white; background-color: #3f4156; padding: 8px; border-radius: 8px;");
+    ui->notes_container->addWidget(note);
 }
 
 
